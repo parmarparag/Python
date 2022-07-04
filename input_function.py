ddb4830648d6d61424392776384b26cc92d8b3d2
Parmar Parag <107787295+parmarparag@users.noreply.github.com>
2022-07-04 11:55:42 +0530

    input function

---
+++ input_function.py
@@ -0,0 +1,18 @@
+#sum of two numbers
+
+# a=input("please enter a first number:")
+# b=input("please enter a second number:")
+
+# print("the sum of two number",int(a)+int(b))
+
+
+# #average of two numbers
+# a=input("please enter a first number:")
+# b=input("please enter a second number:")
+# avg=(int(a)+int(b))/2
+# print("the average of two number",avg)
+
+a=input("Enter a number")
+a=int(a)
+a*=a
+print(a)
