731e43bebb221702093637d1125f5de0e7477f83
Parmar Parag <107787295+parmarparag@users.noreply.github.com>
2022-07-04 11:58:25 +0530

    escape sequence

---
+++ escape_sequences.py
@@ -0,0 +1,5 @@
+
+#escape sequence
+
+print("Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\tHow I wonder what you are!\n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\tHow I wonder what you are")
+	
