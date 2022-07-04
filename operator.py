811d55faadad831413a2ff3bc932706f669c8f2c
Parmar Parag <107787295+parmarparag@users.noreply.github.com>
2022-07-04 11:55:08 +0530

    operator

---
+++ operator.py
@@ -0,0 +1,50 @@
+a=10
+b=15
+
+#arithmetic operator
+
+print("The value of 10+15 is ",10+15)
+print("The value of 10-15 is ",10-15)
+print("The value of 10*15 is ",10*15)
+print("The value of 10/15 is ",10/15)
+
+#assignment operator
+p=5
+q=10
+r=15
+s=20
+
+p+=10
+q-=15
+r*=20
+s/=25
+
+print(p,q,r,s)
+# print(q)
+# print(r)
+# print(s)
+
+#comparision operator
+
+u= (5>7)
+v= (5<7)
+w= (5>=7)
+x= (5<=7)
+y= (5==7)
+z= (5!=7)
+
+print(u)
+print(v)
+print(w)
+print(x)
+print(y)
+print(z)
+
+#logical operator 
+
+bool1=True
+bool2=False
+
+print("The value of bool1 and bool2 is",(bool1 and bool2))
+print("The value of bool1 or bool2 is",(bool1 or bool2))
+print("The value of not bool2 is",(not bool2))
