0534ce957499142363a98b1c400c613cf2e508fa
Parmar Parag <107787295+parmarparag@users.noreply.github.com>
2022-07-04 12:00:57 +0530

    tuple method

---
+++ tuple_methods.py
@@ -0,0 +1,14 @@
+
+#tuple methods
+
+from itertools import count
+
+
+t1=(1,2,3,4,5,6,1,51,12,1)
+print(t1.count(1))
+print(t1.index(51))
+
+#simple tuple program
+
+t1=(7,0,8,0,0,9)
+print(t1.count(0))
