77ac139139c676a86b19afbd3b3230cd863a3c1c
Parmar Parag <107787295+parmarparag@users.noreply.github.com>
2022-07-04 11:59:09 +0530

    list

---
+++ lists.py
@@ -0,0 +1,22 @@
+#create a list using []
+
+l1=[1,2,3,4,5,6,52]
+print(l1)
+
+#accses index using a[0],a[1],a[2]
+
+print(l1[3])
+
+#change the value of list using
+
+l1[4]=200
+print(l1)
+
+#we can create a list with items of different types
+
+l2=[1,2,"parag","Akashtechnolab",20.5,True]
+print(l2)
+
+#list slicing
+l3=["kl Rahul","Kohli","Rohit","Hardik","Dhoni","Chahal"]
+print(l3[0:4])
