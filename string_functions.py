af0943b6c8c611291331cf89126224625fb94586
Parmar Parag <107787295+parmarparag@users.noreply.github.com>
2022-07-04 11:57:39 +0530

    string function

---
+++ string_functions.py
@@ -0,0 +1,14 @@
+var_1="gandhinagar Institute Of Technology Is One Of The Best Collage For Engineering"
+
+#string function 
+
+print(len(var_1))
+
+print(var_1.endswith("kdhfdfh")) #output is false
+print(var_1.endswith("Engineering")) #output is true
+
+print(var_1.count("e"))
+print(var_1.capitalize())
+print(var_1.find("gandhinagar"))
+print(var_1.replace("gandhinagar","ahmedabad"))
+print(var_1.upper())
