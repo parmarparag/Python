456bfd7f43f4ec079609785a1e4a37911d9b1845
Parmar Parag <107787295+parmarparag@users.noreply.github.com>
2022-07-04 12:01:35 +0530

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+mydict={
+    "Parag" : "Founder",
+    "Mayur":"Python Developer",
+    "Dhairya" : "Talk Master",
+    "Nisarg" : "Bussiness Man",
+    "marks" : [12,25]
+}
+mydict["Mayur"]= "Game Developer"
+print(mydict["Dhairya"])
+print(mydict["Parag"])
+print(mydict["Mayur"])
