946f30ccbc3af2c8ba6326801db3c7a94b624936
Parmar Parag <107787295+parmarparag@users.noreply.github.com>
2022-07-04 12:00:28 +0530

    tuple

---
+++ tuples.py
@@ -0,0 +1,20 @@
+# create a tuple
+
+t1=(1,2,3,4,25)
+#print(t1)
+print(t1[1])
+
+#empty tuple
+t1=() 
+print(t1)
+
+# wrong way to declare a tuplewith single element
+t1=(1)
+print(t1)
+
+#tuple with single element
+t1=(1,)
+print(t1)
+
+#immutable means cannot update the value of tuple
+t1[0]=34
