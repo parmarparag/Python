e401d8bb6ed4f56b3e5a6e328d627707cfaeb1e8
Parmar Parag <107787295+parmarparag@users.noreply.github.com>
2022-07-04 11:56:58 +0530

    string slicing

---
+++ string_slicing.py
@@ -0,0 +1,27 @@
+#concatenating  two string
+
+var_1="Good Morning "
+var_2="Parag"
+var_3=var_1+var_2
+print(var_3)
+
+
+#string indexing
+
+a="Mahadev"
+print(a[3])
+
+#string slicing + indices
+
+a="Talk Master"
+print(a[0:11])
+
+#string slicng - indices
+
+a="Talk Master"
+print(a[-6:-5])
+
+#string slicing with skip value
+
+a="Talk Master"
+print(a[0:11:2])
